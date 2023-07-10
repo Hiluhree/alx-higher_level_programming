a657187bf30e18403ac95d3b5b5fe448f4478030
Hillary Cheserek <cheserekhillary@gmail.com>
2023-07-10 06:09:54 -0700

    making corrections

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# 3-is_kind_of_class.py"""Defines a class and inherited class-checking function."""
+# 3-is_kind_of_class.py
+"""Defines a class and inherited class-checking function."""
 
 
 def is_kind_of_class(obj, a_class):
